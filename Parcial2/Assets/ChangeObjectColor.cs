d36847a9c7d12a086657ddfa7b202a178f5452dc
Laura Molina <lauramolinay@gmail.com>
2021-09-23 01:21:33 -0500

    Se crea app Base
    La app permite agregar diferentes objetos a una lista, luego permite cambiarle el color a dichos objetos.
    
    Pendiente: Agregar varias opciones de colores

--- Parcial2/Assets/ChangeObjectColor.cs
+++ Parcial2/Assets/ChangeObjectColor.cs
@@ -4,52 +4,47 @@ using UnityEngine;
 
 public class ChangeObjectColor : MonoBehaviour
 {
-    // Update is called once per frame
-    [SerializeField] private string selectableTag = "Selectable";
-    [SerializeField] private Material highlightMaterial;
-    public Material dMaterial;
+    [SerializeField] private Material chaMaterial; // Material a cambiar
+    [SerializeField] private Material defMaterial; //Material default
 
-    MeshRenderer selectionRenderer;
-    GameObject selection;
-    private GameObject _selection;
+    bool hasChanged = false; //Bool sobre si ha cambiado el objeto de textura.
+    
+    public ManageObjects MO; //Ref al ManageObjects
+    public List<MeshRenderer> renders = new List<MeshRenderer>(); //Lista para almacenar los renders de cada obj en MO.
 
-
-    private void Awake()
-    {
-        dMaterial = gameObject.GetComponent<Renderer>().material;
-        selectionRenderer = gameObject.GetComponent<MeshRenderer>();
+    private void Start()
+    { //Referencias a componentes
+        MO = gameObject.GetComponent<ManageObjects>();
     }
-    void Update()
-    {
-        if (_selection != null)
-        {
-            selectionRenderer.material = dMaterial;
-            _selection = null;
-        }
 
-        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
+    public void HandleColor() //Metodo que se llama desde change Color
+    {       
+        CheckObjects(); //Checkee que objetos se cambiara el color
+        foreach (MeshRenderer render in renders) //Para cada render
         {
-            selection = hit.transform.gameObject;
-            if (selection.CompareTag(selectableTag))
-            {
-                var selectionRenderer = selection.GetComponent<MeshRenderer>();
-                if (selectionRenderer != null)
-                {
-                    selectionRenderer.material = highlightMaterial;
-                }
-                _selection = selection;
-            }
-            if (selection.CompareTag("SelectableExit"))
-            {
-                var selectionRenderer = selection.GetComponent<MeshRenderer>();
-                if (selectionRenderer != null)
-                {
-                    selectionRenderer.material = highlightMaterial;
-                }
-                _selection = selection;
-            }
+            if (!hasChanged) ChangeColor(render); //si no se han cambiado todos siga cambiando colores.
+            else RevertColor(render); //Si ya se cambio y se preciona denuevo el boton, regreselos al original.
         }
+        hasChanged = !hasChanged;
     }
+     void CheckObjects()
+     {    
+        renders.Clear(); //Limpie la lista de Renders antes de agregarlos
+        foreach (GameObject obj in MO.objects) //agregue solo los rendes de los objetos dentro de MO
+        {
+            renders.Add(obj.GetComponent<MeshRenderer>());
+        }
+     }
+
+    //Fucionar estos 2 metodos en una sola cosa para el principio de extender sin modificar
+    void ChangeColor(MeshRenderer render)
+    {
+        render.material = chaMaterial;
+    }
+    void RevertColor(MeshRenderer render)
+    {
+        render.material = defMaterial;
+
+    }
+
 }
